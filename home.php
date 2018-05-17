5d0b422dac790eb9a9412e6c811f98e5163717ee
Matthew Behnke <iterevo@gmail.com>
2018-05-16 23:26:04 -0500

    something quick to test if $_session worked

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php // Initialize the session
+session_start();
+
+// If session variable is not set it will redirect to login page
+
+if(!isset($_SESSION['userid']) || empty($_SESSION['userid'])){
+    header("location: index.php");
+    exit;
+} 
+
+
+include 'head.php';
+
+echo $_SESSION['id'] .'<br>';
+echo $_SESSION['userid'].'<br>';
+echo $_SESSION['username'].'<br>';
+echo $_SESSION['firstname'].'<br>';
+echo $_SESSION['lastname'].'<br>';
+echo $_SESSION['email'].'<br>';
+echo $_SESSION['level'].'<br>';
+
+?>
