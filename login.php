8c44096b3eca3d4b33227a3c158ba297afcdde61
Matthew Behnke <iterevo@gmail.com>
2018-05-16 23:24:45 -0500

    added login page

--- login.php
+++ login.php
@@ -36,7 +36,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
   // Prepare a select statement
 
-    $sql = "SELECT userid, password FROM users WHERE userid = ?";
+    $sql = "SELECT userid, password, ID, firstname, lastname, email, level  FROM users WHERE userid = ?";
 
       if($stmt = mysqli_prepare($database, $sql)){
         // Bind variables to the prepared statement as parameters
@@ -50,14 +50,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
           // Check if userid exists, if yes then verify password
           if(mysqli_stmt_num_rows($stmt) == 1){                    
             // Bind result variables
-            mysqli_stmt_bind_result($stmt, $userid, $hashed_password);
+            mysqli_stmt_bind_result($stmt, $userid, $hashed_password, $ID, $firstname, $lastname, $email, $level);
             if(mysqli_stmt_fetch($stmt)){
               if(password_verify($password, $hashed_password)){
                 /* Password is correct, so start a new session and
                 save the userid to the session */
                 session_start();
-                $_SESSION['userid'] = $userid;      
-                header("location: welcome.php");
+                $_SESSION['id'] = $ID; // setting the sessions id
+                $_SESSION['userid'] = $userid; // setting the sessions user id
+                $_SESSION['username'] = $firstname . $lastname; // setting the username
+                $_SESSION['firstname'] = $firstname; // setting the users first name 
+                $_SESSION['lastname'] = $lastname; // setting the users last name
+                $_SESSION['email'] = $email; // settig the users email
+                $_SESSION['level'] = $level; // setting the users 'level'
+                header('location: home.php');
               } else{
                 // Display an error message if password is not valid
                 $password_err = 'The password you entered was not valid.';
