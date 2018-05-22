585a5dd562d117f4b6ec0d528348ef14514ab30d
jatupper <jatupper@student.linnmar.org>
2018-05-22 13:13:16 -0500

    changes to admin pages

--- admin/students.php
+++ admin/students.php
@@ -127,12 +127,12 @@ while($row=mysqli_fetch_assoc($result))
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Dashboard</h1>
+            <h1 class="h2 text-center">LM Robotics</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
             </div>
           </div>
 
-          <h2>Section title</h2>
+          <h2 class = "text-center">Students</h2>
           <div class="table-responsive">
             <table class="table table-striped table-sm">
               <thead>
