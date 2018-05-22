0b427d63ac1db8b7ca544618886cafebd16d7a2c
jatupper <jatupper@student.linnmar.org>
2018-05-22 13:04:14 -0500

    g

--- admin/coaches.php
+++ admin/coaches.php
@@ -112,7 +112,7 @@ while($row=mysqli_fetch_assoc($result))
               <li class="nav-item">
                 <a class="nav-link" href="#">
                   <span data-feather="file-text"></span>
-                  Students loged In
+                  Students logged In
                 </a>
               </li>
               <li class="nav-item">
@@ -127,12 +127,12 @@ while($row=mysqli_fetch_assoc($result))
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Students</h1>
+            <h1 class="h2 text-center">LM Robotics</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
             </div>
           </div>
 
-          <h2>Section title</h2>
+          <h2 class = "text-center">Coaches</h2>
           <div class="table-responsive">
             <table class="table table-striped table-sm">
               <thead>
