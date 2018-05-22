09c6fc4e6b564e43215a3d0421dd7eaa6d1e5e7e
jatupper <jatupper@student.linnmar.org>
2018-05-22 12:17:51 -0500

    style on unapproved
    style
    no

--- unapproveduser.php
+++ unapproveduser.php
@@ -4,11 +4,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <style>
+    body {
+      background-color:#333;
+      text-align: center;
+    }
+    p {
+      color:#fff;
+    }
+    a {
+      color:#fff;
+    }
+  </style>
   <title>Waiting for Approval</title>
 </head>
 <body>
   <p>We are curently waiting for an admin to approve you and assign you a team</p>
   <p>Please try later or contact an admin</p>
-  <a href="./index.php">home page</a>
+  <a href="./index.php">return to home page</a>
 </body>
 </html>
