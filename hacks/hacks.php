fd2d43fb043a4ab705e5235ef875a5efb22b90f3
Matthew Behnke <iterevo@gmail.com>
2018-05-24 12:51:42 -0500

    some cleanup

--- hacks/hacks.php
+++ hacks/hacks.php
@@ -1,17 +1,17 @@
 <?php
 
-require_once "../database.php"
+require_once "../database.php";
 
 for ($i = 0; $i < 20; $i++){
-    $sql = "INSERT INTO users (userid, firstname, lastname, email, password) VALUES (?,?,?,?,?)"
-    if($stmt_newuser = mysqli_prepare($database, $sql_newuser)){
+    $sql = "INSERT INTO users (userid, firstname, lastname, email, password) VALUES (?,?,?,?,?)";
+    if($stmt_newuser = mysqli_prepare($database, $sql)){
         // Bind variables to the prepared statement as parameters
         mysqli_stmt_bind_param($stmt_newuser, "sssss", $param_userid, $param_firstname ,$param_lastname, $param_email, $param_password);
         // Set parameters
-        $userid = "user{$i}userid";
-        $firstname = "user{$i}fn";
-        $lastname = "user{$i}ln";
-        $email = "user{$i}email@email.com";
+        $param_userid = "user{$i}userid";
+        $param_firstname = "user{$i}fn";
+        $param_lastname = "user{$i}ln";
+        $param_email = "user{$i}email@email.com";
         $param_password = password_hash("123456", PASSWORD_DEFAULT); // Creates a password hash
         // Attempt to execute the prepared statement
         if(mysqli_stmt_execute($stmt_newuser)){
@@ -23,11 +23,4 @@ for ($i = 0; $i < 20; $i++){
     }
   // Close connection
   mysqli_close($database);
-
-
-
-
-
-
-
 ?>
