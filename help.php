0a4936ae59f9d24c3d261603a572833b2507237c
Matthew Behnke <iterevo@gmail.com>
2018-05-24 13:47:36 -0500

    update to help

--- help.php
+++ help.php
@@ -1,12 +1,18 @@
 <?php
-include "head.php";
+
+session_start();
+
+// If session variable is not set it will redirect to login page
+
+if(!isset($_SESSION['userid']) || empty($_SESSION['userid'])){
+    header("location: index.php");
+    exit;
+} elseif($_SESSION['level']  = 0){
+  header("location: index.php");
+    exit;
+}
+
+
+include 'head.php';
 ?>
-<style>
-*{
-    color: #333;
-}
-h3 {
-    color: #fff;
-}
-</style>
-<h3 class = "text-center">Coming soon to a theatre near you. </h3>
+<h3 class = "text-center">Help is coming soon to a theatre near you. </h3>
